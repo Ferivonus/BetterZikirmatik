b10a7ffd5bc684a2e8304478e57493faddb72840
Ferivonus <ferivonus@hotmail.com>
2025-07-02 18:55:47 +0300

    Add project files.

---
+++ ZİKİRMATİKBUTGOOD/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace ZİKİRMATİKBUTGOOD
+{
+    public partial class MainWindow : Window
+    {
+        // This is where we store the current count
+        private int zikirCount = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ZikirButton_Click(object sender, RoutedEventArgs e)
+        {
+            zikirCount++;
+            ZikirLabel.Text = zikirCount.ToString();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            zikirCount = 0;
+            ZikirLabel.Text = "0";
+        }
+    }
+}
